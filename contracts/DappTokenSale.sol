80be9be28c1ddf6c91da40ed8c38c588b49f42cc
AntonStrickland <antonstrickland@protonmail.com>
2018-11-30 05:10:40 -0600

    Token Sale Smoke Test

---
+++ contracts/DappTokenSale.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.2;
+
+import "./DappToken.sol";
+
+contract DappTokenSale {
+
+  address admin;
+  DappToken public tokenContract;
+  uint256 public tokenPrice;
+
+  function DappTokenSale(DappToken _tokenContract, uint256 _tokenPrice) public {
+
+    // Assign an admin
+    admin = msg.sender;
+
+    // Token contract
+    tokenContract = _tokenContract;
+
+    // Token Price
+    tokenPrice = _tokenPrice;
+
+  }
+}
